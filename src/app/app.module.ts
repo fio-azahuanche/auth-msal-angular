6bbdbc9f72bf44413105c3986b7e036f19ec7fb8
Fiorela Azahuanche <fazahuanchef@gmail.com>
2022-09-29 12:21:07 -0500

    install azure/msal-angular, update app.module and app.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,20 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MsalModule, MsalService, MSAL_INSTANCE } from '@azure/msal-angular';
+import { IPublicClientApplication, PublicClientApplication } from '@azure/msal-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+export function MSALInstanceFactory(): IPublicClientApplication {
+  return new PublicClientApplication({
+    auth: {
+      clientId: 'a1d218b9-f4f5-47fd-8749-6bab45ce85a0',
+      redirectUri: 'http://localhost:4200'
+    }
+  })
+}
 
 @NgModule({
   declarations: [
@@ -10,9 +22,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    MsalModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: MSAL_INSTANCE,
+      useFactory: MSALInstanceFactory
+    },
+    MsalService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
