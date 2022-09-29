13c89dc35e45415df726a6d0a5817eb58e9d3369
Fiorela Azahuanche <fazahuanchef@gmail.com>
2022-09-29 15:24:17 -0500

    add two links and update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PublicPageComponent } from './public-page/public-page.component';
+import { RestrictedPageComponent } from './restricted-page/restricted-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'public-page',
+    component: PublicPageComponent
+  },
+  {
+    path: 'restricted-page',
+    component: RestrictedPageComponent
+  },
+  {
+    path: '**',
+    component: PublicPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
